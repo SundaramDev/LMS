be81da1c18f222dfb4f67738d2309fceefa0f664
SundaramDev <sundaramkumar75655@gmail.com>
2026-04-02 23:55:26 +0530

    Multer.js file Updated

--- server/configs/multer.js
+++ server/configs/multer.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 // Configure Multer storage (empty means default settings)
 // By default, Multer stores files in memory with no filename change.
-// You can customize destination and filename if needed.
+// We can customize destination and filename if needed.
 const storage = multer.diskStorage({});
 
 // Create the upload middleware using the defined storage engine
